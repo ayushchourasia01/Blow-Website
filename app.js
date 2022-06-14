4279e43fb3e491a3a38163581f951c3784aee85c
ayushchourasia01 <ayushchourasia01@gmail.com>
2022-06-15 02:34:24 +0530

    Commit 2, Challenge 8 - compose half done

--- app.js
+++ app.js
@@ -15,10 +15,25 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.get("/",function(req,res){
+  res.render("home", {firstpage : homeStartingContent});
+});
 
+app.get("/about",function(req,res){
+  res.render("about", {aboutpage : aboutContent});
+});
 
+app.get("/contact",function(req,res){
+  res.render("contact", {contactpage: contactContent});
+});
+app.get("/compose",function(req,res){
+  res.render("compose");
+});
 
-
+app.post("/compose",function(req,res){
+  var item = req.body.Item;
+  console.log(item);
+})
 
 
 
