3c116c82846c709aa6e72f5e1f02a0b97173d6ad
ayushchourasia01 <ayushchourasia01@gmail.com>
2022-06-15 15:46:05 +0530

    Adding New Blogs

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
+let posts = [];
 
 app.set('view engine', 'ejs');
 
@@ -16,7 +17,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/",function(req,res){
-  res.render("home", {firstpage : homeStartingContent});
+  res.render("home", {firstpage : homeStartingContent, posts : posts});
+  
 });
 
 app.get("/about",function(req,res){
@@ -31,8 +33,12 @@ app.get("/compose",function(req,res){
 });
 
 app.post("/compose",function(req,res){
-  var item = req.body.Item;
-  console.log(item);
+  const post = {
+    title : req.body.PostTitle,
+    body : req.body.PostBody
+  };
+  posts.push(post);
+  res.redirect("/");
 })
 
 
